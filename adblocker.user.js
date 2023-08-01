742c753157d3bc51cae6005b7a1971c65b092966
Eijoh <61121271+Eijoh@users.noreply.github.com>
2023-08-02 00:11:36 +0100

    Update adblocker.user.js

--- adblocker.user.js
+++ adblocker.user.js
@@ -1,44 +1,16 @@
 // ==UserScript==
-// @name     NoAds (NSW) ⭐
-// @updateURL https://noads.top/nsw.user.js
-// @downloadURL https://noads.top/nsw.user.js
-// @include  https://ouo.io/*
-// @include  https://ouo.press/*
-// @include  https://nxbrew.com/*
-// @include  https://cutdl.xyz/*
-// @version  1.4
-// @author   Hipelago
-// @description  Autobypass ouo.io / 1link.club / cutdl
+// @name     
+// @updateURL https://noads.top/adblocker.user.js
+// @downloadURL https://noads.top/adblocker.user.js
+// @include  https://microsoft365.com/*
+// @version  1.1
 // @run-at   document-end
 // @grant    none
 // @require  http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // ==/UserScript==
 /*
 */
-(function() {
-  var si = setInterval;
-  setInterval = function(fn, duration) {
-    if (duration >= 1000) duration = duration === 1000 ? 600 : duration;
-    return si.apply(this, arguments);
-  };
-})();
-setInterval(function () {
-    document.getElementsByClassName("btn btn-main")[0].click();
-}, 2000);
-var links,thisLink;
-links = document.evaluate("//a[@href]",
-    document,
-    null,
-    XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
-    null);
-for (var i=0;i<links.snapshotLength;i++) {
-    var thisLink = links.snapshotItem(i);
-    thisLink.href = thisLink.href.replace('http://1link.club/',
-                                          'https://1link.club/');
-    thisLink.href = thisLink.href.replace('https://1link.club/',
-                                          'https://yes.noads.top/ER.php?!VAR!000!=https://1link.club/');
-}
-$(".btn.btn-success.btn-lg.get-link.disabled").hide()
+$(".officeHome__container").hide()
 setInterval(function () {
     document.getElementsByClassName("btn btn-success btn-lg get-link")[0].click();
 }, 2000);
